28655651f7a13115db539995151ff72266e58226
mbrune <bruneskidev@gmail.com>
2018-10-24 23:19:09 -0400

    Adding dependencies and updating gitignore

---
+++ app.js
@@ -0,0 +1,23 @@
+var vue = require('vue');
+var express = require('express');
+var log4js = requrie
+
+var router = Express.router();
+
+router.get('/about', function(req, res) {
+
+});
+
+router.get('/work', function(req, res) {
+
+});
+
+router.get('/contact', function(req, res) {
+
+});
+
+router.get('/', function(req, res) {
+    app.use('index/indexRouter.js');
+});
+
+module.exports = router;
